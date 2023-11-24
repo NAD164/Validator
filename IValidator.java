8eeb44aecbe2fa1a262e8293d3692a6e0e6c1b2a
NAD164 <95597272+NAD164@users.noreply.github.com>
2023-11-24 09:38:22 +0100

    Delete IValidator.java

--- IValidator.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-
-public interface IValidator {
-    void validate(String number, List<String> failedChecks);
-    int calculateChecksum(String digitsOnly);
-    String formatString(String input);
-
-}
